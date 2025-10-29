57f719cb5fb149db4ee238e97a1e8e4ac8e8f869
leilaweicman <leilaweic@hotmail.com>
2025-10-29 11:04:52 +0100

    feat: return JSON health response instead of plain text

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/HealthController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/HealthController.java
@@ -1,5 +1,6 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
+import cat.itacademy.s04.t01.userapi.models.HealthStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping("/health")
-    public String healthCheck() {
-        return "OK";
+    public HealthStatus healthCheck() {
+        return new HealthStatus("OK");
     }
 }
--- src/main/java/cat/itacademy/s04/t01/userapi/models/HealthStatus.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/models/HealthStatus.java
@@ -1,2 +1,3 @@
-package cat.itacademy.s04.t01.userapi.models;public record HealthStatus() {
-}
+package cat.itacademy.s04.t01.userapi.models;
+
+public record HealthStatus(String status) { }
