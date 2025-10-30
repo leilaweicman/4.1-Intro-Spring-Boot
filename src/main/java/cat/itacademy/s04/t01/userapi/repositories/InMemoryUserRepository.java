fbf1efe3ec1137397d180c51290dc363af3f92ca
leilaweicman <leilaweic@hotmail.com>
2025-10-30 12:36:31 +0100

    feat: implement save() and findAll() methods

--- src/main/java/cat/itacademy/s04/t01/userapi/repositories/InMemoryUserRepository.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/repositories/InMemoryUserRepository.java
@@ -2,19 +2,24 @@ package cat.itacademy.s04.t01.userapi.repositories;
 
 import cat.itacademy.s04.t01.userapi.models.User;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public class InMemoryUserRepository implements UserRepository{
+public class InMemoryUserRepository implements UserRepository {
+
+    private final List<User> users = new ArrayList<>();
+
     @Override
     public User save(User user) {
-        return null;
+        users.add(user);
+        return user;
     }
 
     @Override
     public List<User> findAll() {
-        return null;
+        return new ArrayList<>(users);
     }
 
     @Override
