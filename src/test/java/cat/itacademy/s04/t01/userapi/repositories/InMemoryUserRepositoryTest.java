ade3186af3b9538b0c18b8aa41348c2549d80663
leilaweicman <leilaweic@hotmail.com>
2025-10-30 13:12:50 +0100

    test: create InMemoryUserRepositoryTest class with empty test stubs

---
+++ src/test/java/cat/itacademy/s04/t01/userapi/repositories/InMemoryUserRepositoryTest.java
@@ -0,0 +1,31 @@
+package cat.itacademy.s04.t01.userapi.repositories;
+
+import org.junit.jupiter.api.Test;
+
+public class InMemoryUserRepositoryTest {
+
+    @Test
+    void save_returnsAddedUser() {
+
+    }
+
+    @Test
+    void findAll_returnsAllUsers() {
+
+    }
+
+    @Test
+    void findById_returnsExistingUser() {
+
+    }
+
+    @Test
+    void searchByName_returnsUsersMatchingName() {
+
+    }
+
+    @Test
+    void existsByEmail_returnsTrueIfEmailExists() {
+
+    }
+}
