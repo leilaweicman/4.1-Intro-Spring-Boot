e62921b2b78392189c9fa176b1226f891d9fc226
leilaweicman <leilaweic@hotmail.com>
2025-10-30 10:53:17 +0100

    test: verify GET /users returns empty list initially

---
+++ src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserControllerTest.java
@@ -0,0 +1,40 @@
+package cat.itacademy.s04.t01.userapi.controllers;
+
+import cat.itacademy.s04.t01.userapi.models.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+import java.util.List;
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void getUsers_returnsEmptyListInitially() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+}
