1fbf3093f2a1c35fe2605f67e7c5550f72f0d015
leilaweicman <leilaweic@hotmail.com>
2025-10-30 13:21:54 +0100

    test: implement save_returnsAddedUser test

--- src/test/java/cat/itacademy/s04/t01/userapi/repositories/InMemoryUserRepositoryTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/repositories/InMemoryUserRepositoryTest.java
@@ -1,12 +1,24 @@
 package cat.itacademy.s04.t01.userapi.repositories;
 
+import cat.itacademy.s04.t01.userapi.models.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class InMemoryUserRepositoryTest {
 
+    private InMemoryUserRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        repository = new InMemoryUserRepository();
+    }
+
     @Test
     void save_returnsAddedUser() {
-
+        User user = new User("John", "john@example.com");
+        repository.save(user);
+        assertEquals(1, repository.findAll().size());
     }
 
     @Test
