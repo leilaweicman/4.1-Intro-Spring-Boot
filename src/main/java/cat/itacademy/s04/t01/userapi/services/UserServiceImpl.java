0a7ec0863f46fa9825180e3c059ac58523e264d6
leilaweicman <leilaweic@hotmail.com>
2025-11-03 10:53:14 +0100

    feat: add @Service to UserService

--- src/main/java/cat/itacademy/s04/t01/userapi/services/UserServiceImpl.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/services/UserServiceImpl.java
@@ -2,11 +2,13 @@ package cat.itacademy.s04.t01.userapi.services;
 
 import cat.itacademy.s04.t01.userapi.models.User;
 import cat.itacademy.s04.t01.userapi.repositories.UserRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
